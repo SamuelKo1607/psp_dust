8adcc8308e9aa708c717e7f53876b5cdaf625cff
Samuel Kociscak <s.kociscak@gmail.com>
2024-05-14 17:18:40 +0200

    dust depletion zone initial

---
+++ ddz_profile.py
@@ -0,0 +1,534 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import datetime as dt
+from numba import jit
+from tqdm.auto import tqdm
+from scipy.interpolate import CubicSpline
+from scipy.stats import poisson
+from scipy.signal import argrelextrema
+
+from eccentricity_core import bound_flux_vectorized
+from eccentricity_core import r_smearing
+from eccentricity_core import velocity_verlet
+from ephemeris import get_approaches
+from ephemeris import load_ephemeris
+from load_data import encounter_group
+from conversions import jd2date
+from overplot_with_solo_result import get_detection_errors
+from conversions import GM, AU
+from conversions import date2jd, jd2date
+from load_data import Observation
+from load_data import load_all_obs
+
+from paths import psp_sun_ephemeris_file
+from paths import figures_location
+from paths import all_obs_location
+
+import figure_standards as figstd
+axes_size = figstd.set_rcparams_dynamo(mpl.rcParams, num_cols=1, ls='thin')
+mpl.rcParams['figure.dpi'] = 600
+
+
+
+def plot_maxima_zoom(data,
+                     perihelia,
+                     flux,
+                     e,
+                     max_perihelia=16,
+                     aspect=2,
+                     zoom=1.2,
+                     days=7,
+                     pointcolor="darkorange",
+                     linecolor="black",
+                     filename=None):
+
+    # Getting the approaches
+    approaches = np.linspace(1,16,
+                             16,
+                             dtype=int)
+
+    approach_dates = np.array([jd2date(a)
+                                  for a
+                                  in perihelia])
+    approach_groups = np.array([encounter_group(a)
+                                   for a
+                                   in approaches])
+
+    dates = np.array([jd2date(jd) for jd in data["jd"]])
+    post_approach_threshold_passages = np.array([
+        np.min(dates[(dates>approach_date)
+                     *(data["r_sc"]>0.4)])
+        for approach_date in approach_dates])
+
+    # Calculate the scatter plot
+    point_errors = get_detection_errors(data["measured"])
+    duty_hours = data["obs_time"]
+    detecteds = data["measured"]
+    scatter_point_errors = np.vstack((point_errors[0]
+                                         / data["obs_time"],
+                                      point_errors[1]
+                                         / data["obs_time"]))
+
+    # Caluclate the model line
+    eff_rate_bound = flux
+
+    # Plot
+    fig = plt.figure(figsize=(4*aspect/zoom, 4/zoom))
+    ax1 = plt.subplot2grid(shape=(2,6), loc=(0,0), colspan=2, fig=fig)
+    ax2 = plt.subplot2grid((2,6), (0,2), colspan=2, fig=fig)
+    ax3 = plt.subplot2grid((2,6), (0,4), colspan=2, fig=fig)
+    ax4 = plt.subplot2grid((2,6), (1,1), colspan=2, fig=fig)
+    ax5 = plt.subplot2grid((2,6), (1,3), colspan=2, fig=fig)
+    axes = np.array([ax1,ax2,ax3,ax4,ax5])
+
+    for a in axes[:]:
+        a.set_ylabel("Rate [/h equiv.]")
+    for a in axes[:]:
+        a.set_xlabel("Time after perihelion [h]")
+
+    # Iterate the groups
+    for i,ax in enumerate(axes):  #np.ndenumerate(axes):
+        group = i+1
+        if group in set(approach_groups):
+
+            ax.set_title(f"Enc. group {group}")
+
+            line_hourdiff = np.zeros(0)
+            line_rate = np.zeros(0)
+
+            for approach_date in approach_dates[approach_groups==group]:
+                filtered_indices = np.abs(dates-approach_date
+                                          )<dt.timedelta(days=days)
+                datediff = dates[filtered_indices]-approach_date
+                hourdiff = [24*d.days + d.seconds/3600
+                            for d in datediff]
+                passage_days = (np.max(post_approach_threshold_passages[
+                                        approach_groups==group])
+                                - approach_date)
+                passage_hours = (24*passage_days.days
+                                 + passage_days.seconds/3600)
+                ax.scatter(hourdiff,
+                           (detecteds[filtered_indices]
+                            /duty_hours[filtered_indices]),
+                          c=pointcolor,s=0.,zorder=100)
+                ax.errorbar(hourdiff,
+                            (detecteds[filtered_indices]
+                             /duty_hours[filtered_indices]),
+                            scatter_point_errors[:,filtered_indices],
+                           c=pointcolor, lw=0., elinewidth=1,alpha=0.5)
+
+                line_hourdiff = np.append(line_hourdiff,hourdiff)
+                line_rate = np.append(line_rate,flux[filtered_indices])
+            sortmask = line_hourdiff.argsort()
+
+            ax.plot(line_hourdiff[sortmask],
+                    line_rate[sortmask],
+                    c=linecolor,lw=1,zorder=101,label=f"e = {e}")
+            max_y = ax.get_ylim()[1]
+            ax.vlines([-passage_hours,passage_hours],
+                      0,10000,
+                      color="gray")
+
+            ax.set_ylim(0,max_y)
+            ax.set_xlim(-days*24,days*24)
+
+    fig.tight_layout()
+
+    if filename is not None:
+        fig.savefig(figures_location+filename+".png",dpi=1200)
+
+    fig.show()
+
+
+def load_ephem_data(ephemeris_file,
+                    r_min=0,
+                    r_max=0.35,
+                    decimation=1):
+    """
+    Reads the ephemerides, returns distance, speeds while disregarding 
+    the "z" component.
+
+    Parameters
+    ----------
+    ephemeris_file : str
+        Location of the ephemerides file.
+    r_min : float, optional
+        The minimum (exclusive) heliocentric distance. The default is 0.
+    r_max : float, optional
+        The maximum (exclusive) heliocentric distance. The default is 0.5.
+    decimation : int, optional
+        How much to decimate the data. The default is 4 = every 4th is kept.
+
+    Returns
+    -------
+    data : pd.df
+        All the data which "main" needs.
+
+    """
+
+    # ssb_ephem="psp_ssb_noheader.txt"
+    # sun_ephem="psp_sun_noheader.txt"
+
+    # ssb_ephem_file = os.path.join("data_synced",ssb_ephem)
+    # sun_ephem_file = os.path.join("data_synced",sun_ephem)
+    # sol_ephem_file = os.path.join("data_synced","sun_ssb_noheader.txt")
+
+    # ephem_sun = load_ephemeris(sun_ephem_file)
+    # hae_sun = ephem_sun[1]
+    # x_sun = hae_sun[:,0]/(AU/1000) #AU
+    # y_sun = hae_sun[:,1]/(AU/1000)
+    # z_sun = hae_sun[:,2]/(AU/1000)
+    # hae_v_sun = ephem_sun[2]
+    # vx_sun = hae_v_sun[:,0] #km/s
+    # vy_sun = hae_v_sun[:,1]
+    # vz_sun = hae_v_sun[:,2]
+
+    # ephem_sol = load_ephemeris(sol_ephem_file)
+    # hae_sol = ephem_sol[1]
+    # x_sol = hae_sol[:,0]/(AU/1000) #AU
+    # y_sol = hae_sol[:,1]/(AU/1000)
+    # z_sol = hae_sol[:,2]/(AU/1000)
+    # hae_v_sol = ephem_sol[2]
+    # vx_sol = hae_v_sol[:,0] #km/s
+    # vy_sol = hae_v_sol[:,1]
+    # vz_sol = hae_v_sol[:,2]
+
+    (jd,
+     hae,
+     hae_v,
+     hae_phi,
+     radial_v,
+     tangential_v,
+     hae_theta,
+     v_phi,
+     v_theta) = load_ephemeris(ephemeris_file)
+
+    x = hae[:,0]/(AU/1000) #AU
+    y = hae[:,1]/(AU/1000)
+    z = hae[:,2]/(AU/1000)
+
+    vx = hae_v[:,0] #km/s
+    vy = hae_v[:,1]
+    vz = hae_v[:,2]
+
+    # for i in [3,4]:
+    #     per = perihelia[i]
+    #     indices = indices = np.abs(jd-per)<5
+    #     plt.plot(r_sc[indices],v_tot[indices])
+
+    v_sc_r = np.zeros(len(x))
+    v_sc_t = np.zeros(len(x))
+    for i in range(len(x)):
+        unit_radial = hae[i,0:2]/np.linalg.norm(hae[i,0:2])
+        v_sc_r[i] = np.inner(unit_radial,hae_v[i,0:2])
+        v_sc_t[i] = np.linalg.norm(hae_v[i,0:2]-v_sc_r[i]*unit_radial)
+    r_sc = np.sqrt(x**2+y**2)
+    area_front = np.ones(len(x))*6.11
+    area_side = np.ones(len(x))*4.62
+
+    data = pd.DataFrame(data=np.array([v_sc_r,
+                                       v_sc_t,
+                                       r_sc,
+                                       area_front,
+                                       area_side,
+                                       jd]).transpose(),
+                        index=np.arange(len(r_sc),dtype=int),
+                        columns=["v_sc_r",
+                                 "v_sc_t",
+                                 "r_sc",
+                                 "area_front",
+                                 "area_side",
+                                 "jd"])
+
+    data = data[data["r_sc"]>r_min]
+    data = data[data["r_sc"]<r_max]
+    data = data[data.index % decimation == 0]
+
+    return data
+
+
+def get_detection_errors(counts,
+                         prob_coverage = 0.9):
+    """
+    The function to calculate the errorbars for flux 
+    assuming Poisson distribution and taking into account
+    the number of detections.
+
+    Parameters
+    ----------
+    counts : array of float
+        Counts per day.
+    prob_coverage : float, optional
+        The coverage of the errobar interval. The default is 0.9.
+
+    Returns
+    -------
+    err_plusminus_flux : np.array of float
+        The errorbars, lower and upper bound, shape (2, n). 
+        The unit is [1] (count).
+
+    """
+
+    counts_err_minus = -poisson.ppf(0.5-prob_coverage/2,
+                                          mu=counts)+counts
+    counts_err_plus  = +poisson.ppf(0.5+prob_coverage/2,
+                                          mu=counts)-counts
+    err_plusminus_counts = np.array([counts_err_minus,
+                                   counts_err_plus])
+
+    return err_plusminus_counts
+
+
+def find_approaches(jd,
+                    r,
+                    threshold=0.2):
+    """
+    Finds the jds of approaches (locally lowest r).
+
+    Parameters
+    ----------
+    jd : np.array 1D of float
+        The array of julian dates.
+    r : np.array 1D of float
+        The array of radial distances.
+    threshold : float, optional
+        The maximum r to consider. The default is 0.2.
+
+    Returns
+    -------
+    approaches : TYPE
+        DESCRIPTION.
+
+    """
+
+    local_minima = argrelextrema(r, np.less)[0]
+    approaches = jd[local_minima[r[local_minima]<threshold]]
+    return approaches
+
+
+def most_likely_prefactor(n,
+                          e,
+                          m,
+                          c_min=0.5,
+                          c_max=2,
+                          thershold=1e-5):
+    """
+    Optimization algorithom to find the most likely c. 
+    Assumes positive c.
+
+    Parameters
+    ----------
+    n : np.array 1D of float
+        Detected count.
+    e : np.array 1D of float
+        Exposure.
+    m : np.array 1D of float
+        Model count.
+    c_min : float, optional
+        the seed low c. The default is 0.5.
+    c_max : float, optional
+        the seed high c. The default is 2.
+    thershold : float, optional
+        how precise do we need it. The default is 1e-2.
+
+    Returns
+    -------
+    c : float
+        The most likely multiplicative prefactor.
+
+    """
+    cmax_loglik = np.sum(poisson.logpmf(n,c_max*m*e))
+    cmin_loglik = np.sum(poisson.logpmf(n,c_min*m*e))
+    while (c_max-c_min)>thershold:
+        print(c_min,c_max)
+        c = np.exp((np.log(c_min)+np.log(c_max))/2)
+        c_loglik = np.sum(poisson.logpmf(n,c*m*e))
+
+        if c_loglik > max(cmax_loglik,cmin_loglik):
+            if cmax_loglik > cmin_loglik:
+                c_min = c
+                cmin_loglik = c_loglik
+            else:
+                c_max = c
+                cmax_loglik = c_loglik
+        else:
+            if cmax_loglik > cmin_loglik:
+                c_max = np.exp(np.log(c) + (np.log(c_max) - np.log(c_min)))
+                cmax_loglik = np.sum(poisson.logpmf(n,c_max*m*e))
+                c_min = c
+                cmin_loglik = c_loglik
+            else:
+                c_min = np.exp(np.log(c) - (np.log(c_max) - np.log(c_min)))
+                cmin_loglik = np.sum(poisson.logpmf(n,c_min*m*e))
+                c_max = c
+                cmax_loglik = c_loglik
+    c = (c_min+c_max)/2
+    return c
+
+
+def plot_compare(r_model,
+                 f_model,
+                 r_obs,
+                 f_obs,
+                 errors_obs):
+    """
+    A simple plot of the data and the model at once.
+
+    Parameters
+    ----------
+    r_model : np.array 1D of float
+        Heliocentric distance [AU] in the model.
+    f_model : np.array 1D of float
+        Flux [/s] in the model.
+    r_obs : np.array 1D of float
+        Heliocentric distance [AU] in the data.
+    f_obs : np.array 1D of float
+        Flux [/s] in the data.
+    errors_obs : np.array 2D shape (2,N) of float
+        The errors +- unsigned of the data.
+
+    Returns
+    -------
+    None.
+
+    """
+    fig,ax = plt.subplots()
+    ax.plot(r_model,f_model)
+    ax.errorbar(r_obs,f_obs,
+                errors_obs)
+    ax.vlines([0.02325,0.08836],0,0.1,"k")
+    ax.set_xlim(0,0.2)
+    ax.set_ylim(0,0.1)
+    ax.set_xlabel("Heliocentric distance [AU]")
+    ax.set_ylabel("Dust flux [/s]")
+    plt.show()
+
+
+def scaling_estimate(obs,
+                     rmin=0.125,
+                     rmax=0.25):
+    obs = [ob for ob in obs if (ob.duty_hours > 0.1
+                                and ob.inbound == 1
+                                and ob.heliocentric_distance > rmin
+                                and ob.heliocentric_distance < rmax
+                                )]
+    duty_seconds = np.array([ob.duty_hours for ob in obs])*3600
+    count = np.array([ob.count_corrected for ob in obs])
+    jd = np.array([ob.jd_center for ob in obs])
+    r = np.array([ob.heliocentric_distance for ob in obs])
+
+    # tbd break down into individual approaches
+
+    # tbd fit multiplicative constant for each approach,
+    # and just one slope for all of them, an assistant function needed
+
+
+
+
+
+
+
+
+def main():
+
+    pass
+
+
+
+
+
+
+#%%
+if __name__ == "__main__":
+
+    ephem = load_ephem_data(psp_sun_ephemeris_file)
+    loc = os.path.join(figures_location,"ddz_profile","")
+
+    # evaluate model
+    gamma=-1.3
+    ex=0.15
+    incl=20
+    retro=0.03
+    beta=0.05
+    flux_model = bound_flux_vectorized(
+        r_vector = ephem["r_sc"].to_numpy(),
+        v_r_vector = ephem["v_sc_r"].to_numpy(),
+        v_phi_vector = ephem["v_sc_t"].to_numpy(),
+        S_front_vector = ephem["area_front"].to_numpy(),
+        S_side_vector = ephem["area_side"].to_numpy(),
+        ex = ex,
+        incl = incl,
+        retro = retro,
+        beta = beta,
+        gamma = gamma,
+        n = 7e-9)
+    jd = ephem["jd"].to_numpy()
+    r_sc = ephem["r_sc"].to_numpy()
+    flux_model_spline = CubicSpline(jd, flux_model)
+    r_sc_spline = CubicSpline(jd, r_sc)
+    approaches = find_approaches(jd,r_sc)
+
+    # get what we need from the data
+    psp_obs = load_all_obs(all_obs_location)
+    psp_obs = [ob for ob in psp_obs if ob.duty_hours > 0.1]
+    jds_obs = np.array([ob.jd_center for ob in psp_obs])
+    duty_seconds_obs = np.array([ob.duty_hours for ob in psp_obs])*3600
+    flux_obs = np.array([ob.count_corrected
+                         /ob.duty_hours for ob in psp_obs])/3600
+    flux_obs_errors = np.array([get_detection_errors(ob.count_corrected)/
+                                ob.duty_hours for ob in psp_obs])/3600
+
+    # find the best prefactor for the model
+    all_n = np.zeros(0)
+    all_e = np.zeros(0)
+    all_m = np.zeros(0)
+    for approach in approaches[approaches<max(jds_obs)]:
+        approach_end = min(jd[(jd>approach)*(r_sc>0.2)])
+        flux_obs_part = flux_obs[(jds_obs>approach)
+                                 *(jds_obs<approach_end)]
+        duty_seconds_obs_part = duty_seconds_obs[(jds_obs>approach)
+                                                 *(jds_obs<approach_end)]
+        flux_obs_errors_part = flux_obs_errors[(jds_obs>approach)
+                                               *(jds_obs<approach_end),:]
+        jds_obs_part = jds_obs[(jds_obs>approach)
+                               *(jds_obs<approach_end)]
+        r_obs_part = r_sc_spline(jds_obs_part)
+        f_model_part = flux_model_spline(jds_obs_part)
+        j = jd[(jd>approach)*(jd<approach_end)]
+        r = r_sc[(jd>approach)*(jd<approach_end)]
+        f = flux_model[(jd>approach)*(jd<approach_end)]
+
+        all_n = np.append(all_n,(flux_obs_part[r_obs_part>0.12]*
+                                 duty_seconds_obs_part[r_obs_part>0.12]))
+        all_e = np.append(all_e,duty_seconds_obs_part[r_obs_part>0.12])
+        all_m = np.append(all_m,f_model_part[r_obs_part>0.12])
+        # compare model and the data
+        c = 0.83549
+        plot_compare(r,c*f,
+                     r_obs_part,flux_obs_part,
+                     flux_obs_errors_part.transpose())
+    c = most_likely_prefactor(all_n.astype(int),all_e,all_m)
+    print(c)
+
+    slope = scaling_estimate(load_all_obs(all_obs_location))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
